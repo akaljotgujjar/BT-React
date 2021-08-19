2198c829d9cce5b373e2196c550470b2dc0a481b
Erik Montes <erikmontesdev@gmail.com>
2021-08-19 16:51:16 -0700

    add Day 3 daily challenge solution

--- day3/demoapp/src/App.js
+++ day3/demoapp/src/App.js
@@ -1,57 +1,82 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getMovieDetailsById } from "./utils"; // notice how we don't have to include .js extension
+import { getMoviesBySearchTerm } from "./utils"; // notice how we don't have to include .js extension
 
 import MovieCard from "./components/MovieCard";
 import MovieDetails from "./components/MovieDetails";
+import Spinner from "./components/Spinner";
 
 function App() {
-  const [movie, setMovie] = useState({});
+  const [searchTerm, setSearchTerm] = useState("heat");
+  const [isLoading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
 
   // Since fetch api is something that happens outside of the function where it's invoked, it's considered
   // a side effect, so we need to use inside a useEffect hook
   useEffect(() => {
-    getMovieDetailsById("tt0110357")
-      .then((movie) => {
-        console.log(movie);
 
-        const rating = movie.Ratings?.[0]?.Value;
-        const decimalRating = rating.split('/')?.[0] || rating;
+    setIsLoading(true);
 
-        setMovie({ ...movie, Rating: decimalRating }); // update state for movie
+    getMoviesBySearchTerm(searchTerm)
+      .then((movies) => {
+        console.log(movies);
+
+        setMovies(movies);
+        setError(null);
       })
       .catch((err) => {
+
+        setError(err);
+        setMovies([]);
         console.error("Error:", err);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, []); // empty array, means never execute the effect again, do it only once and that's it
 
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (error) {
+    return (<div>Error</div>);
+  }
+
   return (
-    <div className="App">
-      <h1>Movie App</h1>
-
-
-      {/* Using the MovieCard component */}
-      <MovieCard
-        title={movie.Title}
-        type={movie.Type}
-        posterUrl={movie.Poster}
-      />
-
-      <div className="h-100"></div>
-
-      {/* Using the MovieDetails component */}
-      <MovieDetails
-        posterUrl={movie.Poster}
-        title={movie.Title}
-        rated={movie.Rated}
-        runtime={movie.Runtime}
-        genre={movie.Genre}
-        plot={movie.Plot}
-        actors={movie.Actors}
-        rating={movie.Rating}
-      />
-    </div>
+    <div>Success</div>
   );
+
+
+  // return (
+  //   <div className="App">
+  //     <h1>Movie App</h1>
+
+
+  //     {/* Using the MovieCard component */}
+  //     {/* <MovieCard
+  //       title={movie.Title}
+  //       type={movie.Type}
+  //       posterUrl={movie.Poster}
+  //     /> */}
+
+  //     <div className="h-100"></div>
+
+  //     {/* Using the MovieDetails component */}
+  //     {/* <MovieDetails
+  //       posterUrl={movie.Poster}
+  //       title={movie.Title}
+  //       rated={movie.Rated}
+  //       runtime={movie.Runtime}
+  //       genre={movie.Genre}
+  //       plot={movie.Plot}
+  //       actors={movie.Actors}
+  //       rating={movie.Rating}
+  //     /> */}
+  //   </div>
+  // );
 }
 
 export default App;
