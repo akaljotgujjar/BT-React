f5247a7141cd75b2a2633bae6d1de191449cb2ce
Erik Montes <erikmontesdev@gmail.com>
2021-08-19 12:43:55 -0700

    clean up project, remove boilerplat resources

--- day2/demoapp/src/App.js
+++ day2/demoapp/src/App.js
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { getMoviesBySearchTerm, getMovieDetailsById } from "./utils"; // notice how we don't have to include .js extension
 
@@ -29,20 +28,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
