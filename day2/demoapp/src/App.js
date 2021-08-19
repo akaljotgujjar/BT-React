c262c7c6c9291f54ed167be9a19df8944d178b85
Erik Montes <erikmontesdev@gmail.com>
2021-08-19 13:08:55 -0700

    Day 2 daily challenge

--- day2/demoapp/src/App.js
+++ day2/demoapp/src/App.js
@@ -1,25 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getMoviesBySearchTerm, getMovieDetailsById } from "./utils"; // notice how we don't have to include .js extension
 
+import MovieCard from "./components/MovieCard";
+
+
 function App() {
+  const [movie, setMovie] = useState({});
 
   // Since fetch api is something that happens outside of the function where it's invoked, it's considered
   // a side effect, so we need to use inside a useEffect hook
   useEffect(() => {
     // Test import
-    getMoviesBySearchTerm("heat")
-      .then((movies) => {
-        console.log("Movies found:", movies);
-      })
-      .catch((err) => {
-        console.error("Error:", err);
-      });
+    // getMoviesBySearchTerm("heat")
+    //   .then((movies) => {
+    //     console.log("Movies found:", movies);
+    //   })
+    //   .catch((err) => {
+    //     console.error("Error:", err);
+    //   });
 
     // Test import
     getMovieDetailsById("tt0113277")
       .then((movie) => {
         console.log("Movie Info:", movie);
+
+        setMovie(movie); // update state for movie
       })
       .catch((err) => {
         console.error("Error:", err);
@@ -28,6 +34,11 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Movie App</h1>
+
+      <MovieCard title={movie.Title} type={movie.Type} posterUrl={movie.Poster} />
+
+
     </div>
   );
 }
---
+++ day2/demoapp/src/components/MovieCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MovieCard({ title, type, posterUrl }) {
+    return (
+        <article className="MovieCard">
+            <img src={posterUrl} alt={`${title} Poster`} width="150" />
+
+            <footer className="MovieCard__footer">
+                <h3 className="MovieCard__title">{ title }</h3>
+                <div className="MovieCard__tag">{ type }</div>
+            </footer>
+        </article>
+    )
+}
+
+export default MovieCard;
